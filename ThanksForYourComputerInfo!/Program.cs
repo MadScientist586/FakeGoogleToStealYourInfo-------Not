f1caf47e71bfa96b1405224e743bf28f262893b7
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-16 08:51:27 -0500

    made code better

--- ThanksForYourComputerInfo!/Program.cs
+++ ThanksForYourComputerInfo!/Program.cs
@@ -10,7 +10,7 @@ if (age <= 100 && age > 10)
     Console.WriteLine($"{favColor} is a good color!");
     Console.WriteLine("What is your dream car?");
     var dreamCar = Console.ReadLine();  
-    Console.WriteLine($"{dreamCar} is a nice car!");
+    Console.WriteLine($"{dreamCar}s are nice cars!");
     Console.WriteLine("Last question. What is your favorite brand of computer?");
     var favBrand = Console.ReadLine();
     Console.WriteLine($"{favBrand} is the best computer brand!");
@@ -18,47 +18,93 @@ if (age <= 100 && age > 10)
     var enter = Console.ReadLine();
     Console.WriteLine("Computer error... Would you like to resolve the error?");
     var resolveError = Console.ReadLine();
-    Console.WriteLine("It doesn't matter what you put, this has to be resolved.");
-    Console.WriteLine("Hmmm the error is persistent.");
-    Console.WriteLine("To fix this you have to give me your computer info. Will you give it to me?");
-    var cpuInfo = Console.ReadLine();
-    if (cpuInfo == "Yes" || cpuInfo == "yes")
+    if (resolveError == "yes" || resolveError == "Yes")
     {
-        
-        Console.WriteLine("Thank you! resolving error......");
-        
-        Console.WriteLine("Would you like to continue?");
-        var Continue = Console.ReadLine();
-        if (Continue == "yes" || Continue == "Yes")
+        Console.WriteLine("Resolving error......");
+        Console.WriteLine("Hmmm the error is persistent.");
+        Console.WriteLine("To fix this you have to give me your computer info. Will you give it to me?");
+        var cpuInfo = Console.ReadLine();
+        if (cpuInfo == "Yes" || cpuInfo == "yes")
         {
-            Console.WriteLine("Thanks! Now I have your address, social security number, bank info, computer info, and your license plate!");
+
+            Console.WriteLine("Thank you! resolving error......");
+
+            Console.WriteLine("Would you like to continue?");
+            var Continue = Console.ReadLine();
+            if (Continue == "yes" || Continue == "Yes")
+            {
+                Console.WriteLine("Thanks! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
+
+            else
+            {
+                Console.WriteLine("You don't have a choice! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
         }
-
-
         else
         {
-            Console.WriteLine("You don't have a choice! Now I have your address, social security number, bank info, computer info, and your license plate!"); 
-        }
-           
-    }
-    else
-    {
-        Console.WriteLine("Hmmm I'm going to take it anyways. resolving error......");
-        
-        Console.WriteLine("Would you like to continue?");
-        var Continue = Console.ReadLine();
-        if (Continue == "yes" || Continue == "Yes")
-        {
-            Console.WriteLine("Thanks for being slightly cooperative! Now I have your address, social security number, bank info, computer info, and your license plate!");
-        }
+            Console.WriteLine("Hmmm I'm going to take it anyways. resolving error......");
 
-        else
-        {
-            Console.WriteLine("You don't have a choice! Thanks! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            Console.WriteLine("Would you like to continue?");
+            var Continue = Console.ReadLine();
+            if (Continue == "yes" || Continue == "Yes")
+            {
+                Console.WriteLine("Thanks for being slightly cooperative! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
+            else
+            {
+                Console.WriteLine("You don't have a choice! Thanks! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
         }
-        
     }
    
+  else
+    {
+        Console.WriteLine("I'm going to have to resolve it anyways. Resolving error......");
+        Console.WriteLine("Hmmm the error is persistent.");
+        Console.WriteLine("To fix this you have to give me your computer info. Will you give it to me?");
+        var cpuInfo = Console.ReadLine();
+        if (cpuInfo == "Yes" || cpuInfo == "yes")
+        {
+
+            Console.WriteLine("Thank you! resolving error......");
+
+            Console.WriteLine("Would you like to continue?");
+            var Continue = Console.ReadLine();
+            if (Continue == "yes" || Continue == "Yes")
+            {
+                Console.WriteLine("Thanks! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
+
+            else
+            {
+                Console.WriteLine("You don't have a choice! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Hmmm I'm going to take it anyways. resolving error......");
+
+            Console.WriteLine("Would you like to continue?");
+            var Continue = Console.ReadLine();
+            if (Continue == "yes" || Continue == "Yes")
+            {
+                Console.WriteLine("Thanks for being slightly cooperative! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
+            else
+            {
+                Console.WriteLine("You don't have a choice! Thanks! Now I have your address, social security number, bank info, computer info, and your license plate!");
+            }
+
+        }
+    }
 
 
 }
